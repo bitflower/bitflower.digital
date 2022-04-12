110c0a97d238df92ff15b600c8c4d6c80e0e530e
Matthias Max <matthias.max@bitflower.net>
2022-04-12 12:31:50 +0200

    fix year

--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -110,7 +110,7 @@ export class AppRoot {
                   <stencil-route-link url="/" anchorTitle="bitflower®">
                     <app-icon name="logo" />
                   </stencil-route-link>
-                  <p>© 2021 bitflower®</p>
+                  <p>© {new Date().getFullYear()} bitflower®</p>
                   <ul class="external-links list--unstyled">
                     <li>
                       <a rel="noopener" class="link--external" target="_blank" href="https://twitter.com/bitflowertweets" aria-label="Twitter">
